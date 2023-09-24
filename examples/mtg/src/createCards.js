c3b58325040260c0b24ca3f3291460867ab627cd
Max Clayton Clowes <contact@mcclowes.com>
2023-09-24 17:17:54 +0100

    MTG needs styling

--- examples/mtg/src/createCards.js
+++ examples/mtg/src/createCards.js
@@ -3,11 +3,26 @@ import generateCards from "ccg-card-generator";
 import input from "../cards.config";
 import generateMtgCards from "./generateMtgCards";
 
+const dimensions = {
+  page: {
+    height: 282,
+    width: 216,
+    padding: 5,
+  },
+  card: {
+    height: 88,
+    width: 63,
+    margin: 2,
+    border: 1,
+  },
+};
+
 const options = {
   debug: true,
   style: "./src/styles/mtg.css",
   destination: "./cards.pdf",
   htmlGenerator: generateMtgCards,
+  dimensions,
 };
 
 generateCards(input.cards, options);
